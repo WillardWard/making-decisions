af04a8b400d2addbec008198ce72af87235e0280
Willard Ward <willardwardiii@gmail.com>
2022-04-12 23:03:09 -0500

    Finished problem 11

--- lab.js
+++ lab.js
@@ -131,26 +131,53 @@ let score = 0;
 let passingScore = 7;
 
 /*
-  Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
+  Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. 
+  In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
 */
 
 //CODE HERE
+
+while(score < passingScore){
+  console.log("Your score is not high enough.")
+  score++
+}
 
 ////////// INTERMEDIATE PROBLEMS //////////
 
 ////////// PROBLEM 9 //////////
 /*
   Create a variable called 'changeMyMind' and set it equal to true. 
-  Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false, change the status to true.
+  Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false, 
+  change the status to true.
 
 */
 
 //CODE HERE
 
+let changeMyMind = true
+
+if(changeMyMind === true){
+  changeMyMind = false
+}else{
+  changeMyMind = true
+}
+
+console.log(changeMyMind)
+
 ////////// PROBLEM 10 //////////
-// Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
+// Using the "not" operator (!), change the current value of changeMyMind to true (it 
+// should currently be set to false due to the if-else statement in Problem 9). After you 
+// change the value of changeMyMind, console.log it's new value (it should now read true).
 
 //CODE HERE
+
+if(changeMyMind != true){
+  changeMyMind = true
+}else{
+  changeMyMind = false
+}
+
+console.log(changeMyMind)
 
 ////////// ADVANCED PROBLEMS //////////
 
